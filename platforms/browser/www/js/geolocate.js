633c74b367fa28f0d99487f2077d382ee2315c25
espejor <espejor@gmail.com>
2016-05-28 20:27:07 +0200

    no message

---
+++ platforms/browser/www/js/geolocate.js
@@ -0,0 +1,13 @@
+// Wait for PhoneGap to load
+ //
+ // document.addEventListener("deviceready", onDeviceReady, false);
+ // // PhoneGap is ready
+ // //
+ // function onDeviceReady() {
+ //   navigator.geolocation.getCurrentPosition(onSuccess, onError);
+ // }
+ // onSuccess Geolocation
+ //
+
+  // onError Callback receives a PositionError object
+  //
